af9301001955a722d45188cdf389aa16e7f6007c
paulshim100 <74570815+paulshim100@users.noreply.github.com>
2021-01-26 00:15:21 -0800

    Update workplace.py

--- workplace.py
+++ workplace.py
@@ -1,13 +1,2 @@
 def movieTickets(groups): 
   # write your code here
-  ans = ""
-
-  for group in groups:
-    adults, teens, children = list(map(int, group.split()))
-
-    if adults > 1: 
-      children = 0
-
-    ans += str(adults * 25 + teens * 15 + children * 5) + "\n"
-
-  return ans
